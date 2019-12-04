15c656acd408d8871ec8301e92036789f84d0323
Default user <l.wandurraga@uniandes.edu.co>
2019-12-04 17:40:54 +0000

    ejercicio16

---
+++ ejercicio16.py
@@ -0,0 +1,19 @@
+#ideas de https://github.com/ComputoCienciasUniandes/FISI2028-201920/blob/master/ejercicios/19/JaimeForero_Ejercicio19.py y del mio entregado esa clase
+import numpy as np
+import matplotlib.pylab as plt
+datos=np.loadtxt("month.dat")
+
+def four(datos):
+	n=len(datos)
+	x=np.zeros(n, dtype=complex)
+	for i in range(n):
+		x[i]=0.0j
+		for y in range(n):
+			x[i]+=datos[y]*np.exp(-2.0*np.pi*1.0j/n)**(i*n)
+	return x
+a=four(datos[:200,3])
+b=datos[:200,0]
+c=np.linspace(0,14*np.pi,1152)
+plt.scatter(datos[:,0],datos[:,3], s=2)
+plt.plot(datos[:,0],50*np.sin(c)+50)
+plt.savefig("solar.png")
